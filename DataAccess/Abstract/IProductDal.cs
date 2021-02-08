6dee08f467968f70e16587f4865bf1727434bb8f
Abdullah Kara <kara_abdullah34@hotmail.com>
2021-02-08 14:22:25 +0300

    30 Ocak Ders Sonu

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -11,8 +11,8 @@ namespace DataAccess.Abstract
         void Add(Product product);
         void Update(Product product);
         void Delete(Product product);
-
         List<Product> GetAllByCategory(int categoryId);
+
 
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -12,14 +12,14 @@ namespace DataAccess.Concrete.InMemory
         List<Product> _products;
         public InMemoryProductDal()
         {
-            //Oracle, Sql server, Postgres, MongoDb
-            _products = new List<Product> {
+            _products = new List<Product>
+            {
+                //Oracle, Sql Server, Postgres, MongoDb
                 new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15, UnitsInStock=15 },
                 new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500, UnitsInStock=3 },
                 new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitsInStock=2 },
                 new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=150, UnitsInStock=65 },
-                new Product{ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice=85, UnitsInStock=1 },
-
+                new Product{ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice=85, UnitsInStock=1 }
             };
         }
         public void Add(Product product)
@@ -31,9 +31,9 @@ namespace DataAccess.Concrete.InMemory
         {
             //LINQ - Language Integrated Query
             //Lambda
-            Product productToDelete  = _products.SingleOrDefault(p=>p.ProductId == product.ProductId); 
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId == product.ProductId);
 
-            _products.Remove(productToDelete);
+            _products.Remove(product);
         }
 
         public List<Product> GetAll()
@@ -41,23 +41,20 @@ namespace DataAccess.Concrete.InMemory
             return _products;
         }
 
-        public List<Product> GetAllByCategory(int categoryId)
-        {
-            _products.Where(p => p.CategoryId == categoryId).ToList();
-        }
-
         public void Update(Product product)
         {
-            //Gönderdiğim ürün id'sine sahip olan ürünü bul
+            //Gönderdiğim ürün id'sine sahip olan listedeki ürünü bul
             Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
             productToUpdate.ProductName = product.ProductName;
             productToUpdate.CategoryId = product.CategoryId;
             productToUpdate.UnitPrice = product.UnitPrice;
             productToUpdate.UnitsInStock = product.UnitsInStock;
         }
+        
         public List<Product> GetAllByCategory(int categoryId)
         {
-            _products.Where(p => p.CategoryId == categoryId).ToList();
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
         }
     }
 }
+ 
