048b71611da60a137439739a4b1a863f2007f45f
Abdullah Kara <kara_abdullah34@hotmail.com>
2021-03-02 00:00:46 +0300

    24 Şubat 2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleUI
             ProductTest();
             //IoC
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach (var category in categoryManager.GetAll())
+            foreach (var category in categoryManager.GetAll().Data)
             {
                 Console.WriteLine(category.CategoryName);
             }
@@ -24,7 +24,8 @@ namespace ConsoleUI
 
         private static void ProductTest()
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal()
+                ,new CategoryManager(new EfCategoryDal()));
 
             var result = productManager.GetProductDetails();
 
